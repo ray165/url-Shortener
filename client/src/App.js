9166368156aacc1de40b4ff6b5fcaa4df5119e33
Raymond <70726900+ray165@users.noreply.github.com>
2021-06-11 04:34:44 -0700

    fix sub gitfolder

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React, { useRef } from "react";
+import { TextField, Button } from "@material-ui/core";
+
+export default function  App() {
+  const link = useRef('testLink')
+  // AJAX request to server side on button click. 
+  const send = (e) => {
+    e.preventDefault()
+    console.log(link.current.value)
+  }
+
+
+  return (
+    <>
+      <form noValidate autoComplete="off">
+        <TextField id="outlined-basic" label="your url goes here" variant="outlined" inputRef={link}/>
+      </form>
+      <Button variant="contained" color="primary" onClick={send}>
+        Get Shortened URL
+      </Button>
+    </>
+  );
+}
